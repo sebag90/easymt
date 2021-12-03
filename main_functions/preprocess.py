778396b0cf374e3688449e9923b5de8e9f34336b
sebag90 <seba.g90@gmail.com>
2021-12-04 00:22:09 +0100

    preprocessing functions more clear

--- main_functions/preprocess.py
+++ main_functions/preprocess.py
@@ -16,50 +16,63 @@ class PreprocessPipeline:
         self.bpe = bpe
         self.max_len = max_len
         self.single_file = single_file
-        self.cpu = os.cpu_count()
+        self.cpu = os.cpu_count() / 2
 
     def dexml(self):
-        command = (
-            f"perl preprocessing-tools/de-xml.perl"
-            f" data/{self.name} {self.l1} {self.l2} dexmled"
-        )
+        script = Path(f"preprocessing-tools/de-xml.perl")
+        infile = Path(f"data/{self.name}")
+        ofile = Path(f"data/dexmled")
 
+        # execute command
+        command = (
+            f"perl {script} {infile} {self.l1} {self.l2} {ofile}"
+        )
         os.system(command)
 
-        os.rename(f"dexmled.{self.l1}", f"data/dexmled.{self.l1}")
-        os.rename(f"dexmled.{self.l2}", f"data/dexmled.{self.l2}")
-
+        # return files to be deleted
         return f"data/dexmled.{self.l1}", f"data/dexmled.{self.l2}"
 
     def clean(self):
+        script = Path(f"preprocessing-tools/clean_corpus.perl")
+        infile = Path(f"data/dexmled")
+        ofile = Path(f"data/clean")
+
+        # execute command
         command = (
-            f"perl preprocessing-tools/clean_corpus.perl "
-            f"data/dexmled {self.l1} {self.l2} clean 1 {self.max_len}"
+            f"perl {script} {infile} {self.l1} {self.l2} {ofile} 1 {self.max_len}"
         )
         os.system(command)
-        os.rename(f"clean.{self.l1}", f"data/clean.{self.l1}")
-        os.rename(f"clean.{self.l2}", f"data/clean.{self.l2}")
+
+        # return files to be deleted
         return f"data/clean.{self.l1}", f"data/clean.{self.l2}"
 
     def normalize(self, language):
-        command = (
-            f"perl preprocessing-tools/normalize-punctuation.perl"
-            f" -l {language} < data/clean.{language}"
-            f" > data/{self.name}.norm.{language}"
-        )
+        script = Path(f"preprocessing-tools/normalize-punctuation.perl")
+        infile = Path(f"data/clean.{language}")
+        ofile = Path(f"data/{self.name}.norm.{language}")
 
+        # execute command
+        command = (
+            f"perl {script} -l {language} < {infile} > {ofile}"
+        )
         os.system(command)
+
+        # return files to be deleted
         return f"data/{self.name}.norm.{language}"
 
     def tokenize(self, language):
-        command = (
-            f"perl preprocessing-tools/tokenizer.perl -l {language}"
-            f" -no-escape -threads {self.cpu} < "
-            f"data/{self.name}.norm.{language}"
-            f" > data/{self.name}.tok.{language}"
-        )
+        script = Path(f"preprocessing-tools/tokenizer.perl")
+        infile = Path(f"data/{self.name}.norm.{language}")
+        ofile = Path(f"data/{self.name}.tok.{language}")
 
+        # execute command
+        command = (
+            f"perl {script} -l {language} -no-escape -threads {self.cpu} "
+            f"< {infile} > {ofile}"
+        )
         os.system(command)
+
+        # return files to be deleted
         return f"data/{self.name}.tok.{language}"
 
     def train_truecase(self, language):
@@ -70,23 +83,28 @@ class PreprocessPipeline:
         # if model does not exists, train a new one
         model = Path(f"{folder}/model.{language}")
         if not os.path.isfile(model):
-            command = (
-                f"perl preprocessing-tools/train-truecaser.perl "
-                f"-corpus data/{self.name}.tok.{language}"
-                f" -model {model}"
-            )
+            script = Path(f"preprocessing-tools/train-truecaser.perl")
+            infile = Path(f"data/{self.name}.tok.{language}")
 
+            # execute command
+            command = (
+                f"perl {script} -corpus {infile} -model {model}"
+            )
             os.system(command)
 
     def true_case(self, language):
-        command = (
-            f"perl preprocessing-tools/truecase.perl -model "
-            f"data/truecasing_models/model.{language}"
-            f" <  data/{self.name}.tok.{language} "
-            f"> data/{self.name}.true.{language}"
-        )
+        script = Path(f"preprocessing-tools/truecase.perl")
+        model = Path(f"data/truecasing_models/model.{language}")
+        infile = Path(f"data/{self.name}.tok.{language}")
+        ofile = Path(f"data/{self.name}.true.{language}")
 
+        # execute command
+        command = (
+            f"perl {script} -model {model} < {infile} > {ofile}"
+        )
         os.system(command)
+
+        # return files to be deleted
         return f"data/{self.name}.true.{language}"
 
     def learn_bpe(self, language):
@@ -97,21 +115,26 @@ class PreprocessPipeline:
         # if model does not exists train a new one
         model = Path(f"{folder}/model.{self.bpe}.{language}")
         if not os.path.isfile(model):
-            command = (
-                f"subword-nmt learn-bpe -s {self.bpe} "
-                f"< data/{self.name}.true.{language} "
-                f"> {model}"
-            )
+            infile = Path(f"data/{self.name}.true.{language}")
 
+            # execute command
+            command = (
+                f"subword-nmt learn-bpe -s {self.bpe} < {infile} > {model}"
+            )
             os.system(command)
 
     def bpe_splitting(self, language):
-        command = (
-            f"subword-nmt apply-bpe -c data/subword_models/model.{self.bpe}.{language}"
-            f" < data/{self.name}.true.{language} > "
-            f"data/{self.name}.bpe.{language}")
+        model = Path(f"data/subword_models/model.{self.bpe}.{language}")
+        infile = Path(f"data/{self.name}.true.{language}")
+        ofile = Path(f"data/{self.name}.bpe.{language}")
 
+        # execute command
+        command = (
+            f"subword-nmt apply-bpe -c {model} < {infile} > {ofile}"
+        )
         os.system(command)
+
+        # return files to be deleted
         return f"data/{self.name}.bpe.{language}"
 
     def clean_join(self):
@@ -176,7 +199,18 @@ class PreprocessPipeline:
 def preprocess(args):
     config = Parameters.from_config(args.path)
 
+    # MULTI
     if not args.single:
+        # make sure files exist
+        files = [
+            Path(f"data/{config.dataset.name}.{config.dataset.source}"),
+            Path(f"data/{config.dataset.name}.{config.dataset.target}")
+        ]
+
+        for filename in files:
+            if not os.path.exists(filename):
+                raise FileNotFoundError(f"Missing File: {filename}")
+
         # process 2 languages in parallel
         pipeline = PreprocessPipeline(
             config.dataset.name,
@@ -187,7 +221,11 @@ def preprocess(args):
         )
         pipeline.multi()
 
+    # SINGLE
     else:
+        # make sure file exists
+        if not os.path.isfile(args.single):
+            raise FileNotFoundError(f"Missing File: {args.single}")
         # process one single file
         name = args.single.split(os.sep)[-1]
         name = re.match(r"(.*)\.", name).group(1)
