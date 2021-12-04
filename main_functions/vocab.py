a3bde027d32bc82a9d24384b4c805f06bba93814
sebag90 <seba.g90@gmail.com>
2021-12-04 15:43:39 +0100

    prettyprint

--- main_functions/vocab.py
+++ main_functions/vocab.py
@@ -24,7 +24,7 @@ def build_vocab(args):
                 voc1.add_sentence(l1)
                 voc2.add_sentence(l2)
 
-            print(f"Building vocabulary: line: {i + 1}", end="\r")
+            print(f"Building vocabulary: line {i + 1}", end="\r")
 
             if n_sample != 0:
                 if i > n_sample:
