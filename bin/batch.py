50919bf0688c997fe86e24936efe493e7a31e971
seba <seba.g90@gmail.com>
2022-06-18 10:46:04 +0200

    new name for command line

---
+++ bin/batch.py
@@ -0,0 +1,49 @@
+"""
+Train dataset will be converted to to bytes.
+Each file contains N batches (already converted to indeces).
+N can be modified. This is useful to reduce memory
+requirements during training since files are loaded
+sequentially.
+"""
+
+from pathlib import Path
+
+from utils.dataset import DataLoader
+from utils.parameters import Parameters
+
+
+def main(args):
+    print("Starting: Batching dataset")
+
+    # read data from configuration file
+    config = Parameters.from_config(args.path)
+
+    # load entire dataset
+    train_data = DataLoader.from_files(
+        config.data.src_train,
+        config.data.tgt_train,
+        config.model.max_length,
+        config.training.batch_size
+    )
+
+    outputfile = Path(args.output)
+    lines = 0
+    with open(outputfile, "w", encoding="utf-8") as ofile:
+        for batch in train_data:
+            for src, tgt in zip(*batch):
+                s_sen = " ".join(src)
+                t_sen = " ".join(tgt)
+                ofile.write(f"{s_sen}\t{t_sen}\n")
+                lines += 1
+
+                if lines % 100000 == 0:
+                    print(f"Processed lines: {lines:,}", flush=True)
+
+    if args.max != 0:
+        max_file = Path(f"{outputfile}.max")
+        with open(max_file, "w", encoding="utf-8") as ofile:
+            for pair in train_data.n_longest(args.max):
+                ofile.write(f"{pair.src}\t{pair.tgt}\n")
+
+
+    print("Complete: Batching dataset")
---
+++ bin/vocab.py
@@ -0,0 +1,32 @@
+"""
+reads a file and creates a vocuabulary file
+(TSV: word count). Words are listed in descending
+order. Minimum frequency can be enforced.
+"""
+
+import sys
+
+from utils.lang import Vocab
+
+
+def main(args):
+    if args.progress is True:
+        sys.stderr.write(f"Starting: Building vocabulary\n")
+    
+    voc = Vocab(args.min_freq)
+    for i, line in enumerate(sys.stdin):
+        voc.add_sentence(line.strip())
+
+        if args.progress is True:
+            if (i+1) % 100000 == 0:
+                sys.stderr.write(f"Processed lines: {i + 1:,}\n")
+
+        if args.n_sample != 0:
+            if i > n_sample:
+                break
+
+    for word, count in voc.get_vocab():
+        sys.stdout.write(f"{word}\t{count}\n")
+
+    if args.progress is True:
+        sys.stderr.write("Complete: Building vocabulary\n")
